9134ade02015127907cd8ad9b143899a98318ef8
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-04 16:50:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster.each do |key, val|
+         if @roster.keys == grade
+            @roster[key] << student
+         else
+            @roster[grade] = []
+            @roster[grade] << val
+         end
+     end
+  end
+
+end
