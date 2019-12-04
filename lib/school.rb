bd972cff8909135bfbb5809e4cc0babeb0be7e1d
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-04 16:55:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,13 +9,13 @@ attr_accessor :name, :roster
   def add_student(student, grade)
     @roster.each do |key, val|
       if @roster[grade]
-          @roster[grade] << student 
+          @roster[grade] << student
        else
           @roster[grade] = []
           @roster[grade] << student
        end
          end
-     end 
+     end
   end
 
 end
